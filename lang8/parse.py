8eb6e411fde3c5f61f34a166af20d137654defe2
Joshua Tanner <mindful.jt@gmail.com>
2020-03-26 20:52:05 -0700

    better parsing

---
+++ lang8/parse.py
@@ -0,0 +1,83 @@
+import spacy
+import re
+import itertools
+from spacy.tokens import Doc
+
+
+operation_markers = {
+    '[f-red]': '[/f-red]',
+    '[f-blue]': '[/f-blue]',
+    '[sline]': '[/sline]',
+}
+
+operations_list = list(operation_markers.keys()) + list(operation_markers.values())
+operations_regex = re.compile('(' + '|'.join(re.escape(op) for op in operations_list) + ')')
+Doc.set_extension("corrections", default=[])
+
+
+def extract_corrections(text):
+    matches = list(operations_regex.finditer(text))
+
+    string_pieces = []
+    corrections = []
+
+    last_index = 0
+    offset = 0
+    for i in range(0, len(matches), 2):
+        start_match = matches[i]
+        end_match = matches[i+1]
+
+        start_span = start_match.span()
+        end_span = end_match.span()
+
+        previous_text = text[last_index:start_span[0]]
+        inner_text = text[start_span[1]:end_span[0]]
+
+        left_padding = sum(1 for _ in itertools.takewhile(str.isspace,inner_text))
+        right_padding = sum(1 for _ in itertools.takewhile(str.isspace,reversed(inner_text)))
+
+        offset_update = len(start_match.group(0)) + len(end_match.group(0))
+
+        correction_start = start_span[0] + left_padding - offset
+        correction_end = end_span[1] - right_padding - offset - offset_update
+        offset += offset_update
+
+        last_index = end_span[1]
+        corrections.append((start_match.group(0), correction_start, correction_end))
+        string_pieces.extend([previous_text, inner_text])
+
+    string_pieces.append(text[last_index:len(text)])
+
+    return "".join(string_pieces).strip(), corrections
+
+
+def corrections_tokenizer(tokenizer):
+    def tokenize(text):
+        clean_text, corrections = extract_corrections(text)
+        doc = tokenizer(clean_text)
+        doc_corrections = []
+        for cor in corrections:
+            token_starts = [t.idx for t in doc]
+            start = -1
+            end = -1
+            for index, char_location in enumerate(token_starts):
+                if char_location <= cor[1]:
+                    start = char_location
+                if char_location <= cor[2]:
+                    end = char_location + len(doc[index])
+
+                if char_location > cor[1] and char_location > cor[2]:
+                    break
+
+            doc_corrections.append((doc.char_span(start, end), cor[0]))
+
+        doc._.corrections = doc_corrections
+        return doc
+
+    return tokenize
+
+
+def english_nlp():
+    nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser", "ner"])
+    nlp.tokenizer = corrections_tokenizer(nlp.tokenizer)
+    return nlp
--- test.py
+++ test.py
@@ -1,17 +1,39 @@
-from lang8_data import load, parse
+from lang8 import load, parse
 import unittest
+
 
 class TestEnglishNlp(unittest.TestCase):
     nlp = parse.english_nlp()
 
     def test_correction_handling(self):
-        doc = TestEnglishNlp.nlp("So, as the winter is coming, I'm [f-blue]starting[/f-blue] to feel [f-red]better[/f-red].")
+        str1 = "So, as the winter is coming, I'm [f-blue] really starting [/f-blue] to feel [f-red]better [/f-red]."
+        str2 = "I like big dogs."
+        str3 = "Will you buy me a [sline]small[/sline] dog?"
+        str4 = "'I w[f-blue]ould[/f-blue] appreciate it if you [f-blue]could [/f-blue]correct my sentences.'"
+        doc1 = TestEnglishNlp.nlp(str1)
+        doc2 = TestEnglishNlp.nlp(str2)
+        doc3 = TestEnglishNlp.nlp(str3)
+        doc4 = TestEnglishNlp.nlp(str4)
+
+        self.assertEqual(doc1.text, parse.operations_regex.sub('', str1))
+        self.assertEqual(doc2.text, parse.operations_regex.sub('', str2))
+        self.assertEqual(doc3.text, parse.operations_regex.sub('', str3))
+        self.assertEqual(doc4.text, parse.operations_regex.sub('', str4))
+
+        self.assertEqual([(x[0].text, x[1]) for x in doc1._.corrections],
+                         [('really starting', '[f-blue]'), ('better', '[f-red]')])
+        self.assertEqual([(x[0].text, x[1]) for x in doc2._.corrections], [])
+        self.assertEqual([(x[0].text, x[1]) for x in doc3._.corrections], [('small', '[sline]')])
+        self.assertEqual([(x[0].text, x[1]) for x in doc4._.corrections],
+                         [(None, '[f-blue]'), ('could', '[f-blue]')])
+
+
+        print('woogity')
 
 
 
 def main():
-    x = load.load_data('head.dat')
-    print('wiggity')
+    unittest.main()
 
 if __name__ == '__main__':
     main()
